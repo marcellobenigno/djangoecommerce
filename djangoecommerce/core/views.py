a28ae1c694937a792a2565c7343af8b5af581400
marcellobenigno <benigno.marcello@gmail.com>
2017-01-25 17:02:36 -0300

    adiciona templates

--- djangoecommerce/core/views.py
+++ djangoecommerce/core/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 
 
-def home(request):
+def index(request):
     return render(request, 'index.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def product_list(request):
+    return render(request, 'product_list.html')
+
+
+def product(request):
+    return render(request, 'product.html')
--- djangoecommerce/urls.py
+++ djangoecommerce/urls.py
@@ -18,6 +18,9 @@ from django.contrib import admin
 from .core import views
 
 urlpatterns = [
-    url(r'^$', views.home, name='home'),
+    url(r'^$', views.index, name='index'),
+    url(r'^contato/$', views.contact, name='contact'),
+    url(r'^produto/$', views.product, name='product'),
+    url(r'^produtos/$', views.product_list, name='product_list'),
     url(r'^admin/', admin.site.urls),
 ]
