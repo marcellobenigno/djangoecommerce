0216a98e381e975f07e6c69023c808dbae6d1b22
marcellobenigno <benigno.marcello@gmail.com>
2017-01-29 08:33:24 -0300

    cria filtros

--- djangoecommerce/catalog/admin.py
+++ djangoecommerce/catalog/admin.py
@@ -5,6 +5,7 @@ from .models import Category, Product
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug', 'created', 'modified']
     search_fields = ['name', 'slug']
+    list_filter = ['created', 'modified']
     prepopulated_fields = {'slug': ('name',), }
 
 
@@ -14,6 +15,7 @@ admin.site.register(Category, CategoryAdmin)
 class PrductAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug', 'created', 'modified']
     search_fields = ['name', 'slug', 'category__name']
+    list_filter = ['created', 'modified']
     prepopulated_fields = {'slug': ('name',), }
 
 
