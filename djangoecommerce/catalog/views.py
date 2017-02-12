d9521efc8f8bc6488f728d04b69d4a7faa1027f8
marcellobenigno <benigno.marcello@gmail.com>
2017-02-12 08:09:27 -0300

    adiciona custom template tags e paginação

--- djangoecommerce/catalog/views.py
+++ djangoecommerce/catalog/views.py
@@ -7,6 +7,7 @@ from .models import Product, Category
 class ProductListView(generic.ListView):
     model = Product
     template_name = 'catalog/product_list.html'
+    paginate_by = 3
 
 
 product_list = ProductListView.as_view()
@@ -15,6 +16,7 @@ product_list = ProductListView.as_view()
 class CategoryListView(generic.ListView):
     template_name = 'catalog/category.html'
     context_object_name = 'product_list'
+    paginate_by = 3
 
     def get_queryset(self):
         return Product.objects.filter(category__slug=self.kwargs['slug'])
